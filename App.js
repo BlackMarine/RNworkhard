cd39be6c9f9359c1fc59f27c476ac1ccdd4ecd9e
BlackMarine <wkdeo0000@gmail.com>
2022-01-13 15:56:57 +0900

    Touchable, Pressable 사용해보기 터치시 반응기능
    다음학습 : 스위칭 버튼, textInput

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, 
+  Text, 
+  View, 
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableWithoutFeedback //화면에 가장 위에서 일어나는 탭 이벤트를 listen (UI변화x)
+} from "react-native";
+import { theme } from "./color";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Text style={styles.btnText}>Work</Text>
+        </TouchableOpacity>
+        <TouchableHighlight
+          underlayColor="#DDDDDD"
+          activeOpacity={0.5}
+          onPress={() => console.log("누름!")}
+        >
+          <Text style={styles.btnText}>Travel</Text>
+        </TouchableHighlight>
+      </View>
     </View>
   );
 }
@@ -13,8 +31,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: theme.bg,
+    paddingHorizontal: 60,
+  },
+
+  header: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginTop: 100,
+  },
+
+  btnText: {
+    fontSize: 34,
+    fontWeight: "600",
+    color: "white",
   },
 });
