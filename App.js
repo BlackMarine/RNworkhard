332bfc7ac17b9dc94d6d49556ff332880dd35dbc
BlackMarine <wkdeo0000@gmail.com>
2022-01-13 16:45:09 +0900

    TextInput keyboardType="email-address"

--- App.js
+++ App.js
@@ -4,26 +4,33 @@ import { StyleSheet,
   View, 
   TouchableOpacity,
   TouchableHighlight,
-  TouchableWithoutFeedback //화면에 가장 위에서 일어나는 탭 이벤트를 listen (UI변화x)
+  TouchableWithoutFeedback,
+  TextInput //화면에 가장 위에서 일어나는 탭 이벤트를 listen (UI변화x)
 } from "react-native";
 import { theme } from "./color";
+import React, { useState } from 'react';
 
 export default function App() {
+  const [working, setWorking] = useState(true);
+  const travel = () => setWorking(false);
+  const work = () => setWorking(true);
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <View style={styles.header}>
-        <TouchableOpacity>
-          <Text style={styles.btnText}>Work</Text>
+        <TouchableOpacity onPress={work}>
+          <Text style={{...styles.btnText, color: working ? "white" : theme.grey }}>Work</Text>
         </TouchableOpacity>
-        <TouchableHighlight
-          underlayColor="#DDDDDD"
-          activeOpacity={0.5}
-          onPress={() => console.log("누름!")}
-        >
-          <Text style={styles.btnText}>Travel</Text>
-        </TouchableHighlight>
+        <TouchableOpacity onPress={travel}>
+          <Text style={{...styles.btnText, color: !working ? "white" : theme.grey }}>travel</Text>
+        </TouchableOpacity>
       </View>
+        <View>
+          <TextInput keyboardType="email-address" placeholder={working ? "Add a to Do" : "where do you want to go"} style={styles.input}>
+            
+          </TextInput>
+        </View>
     </View>
   );
 }
@@ -44,6 +51,14 @@ const styles = StyleSheet.create({
   btnText: {
     fontSize: 34,
     fontWeight: "600",
-    color: "white",
   },
+
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    marginTop: 20,
+    fontSize: 18,
+  }
 });
