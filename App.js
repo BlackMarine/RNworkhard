c14e2317946dc2d606a2ae7e428171eec1152e80
BlackMarine <wkdeo0000@gmail.com>
2022-01-17 15:21:05 +0900

    #3.2 TextInput 여러가지 방법
    이제 입력한 값을 저장하는 방법을 학습 예정

--- App.js
+++ App.js
@@ -2,17 +2,18 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, 
   Text, 
   View, 
-  TouchableOpacity,
+  TouchableOpacity, //가장 쉽게 work travel 나눠가고 text input까지만 ㅂ
   TouchableHighlight,
-  TouchableWithoutFeedback,
-  TextInput //화면에 가장 위에서 일어나는 탭 이벤트를 listen (UI변화x)
+  TouchableWithoutFeedback, //그래픽 UI 변화없이 
+  TextInput //화면에 가장 위에서 일어나는 탭 이벤트를 listen (UI변화x) 애니메이션만없음
+  // 1)텍스트 인풋 import 2)<TextInput> 추가
 } from "react-native";
 import { theme } from "./color";
 import React, { useState } from 'react';
 
 export default function App() {
-  const [working, setWorking] = useState(true);
-  const [text, setText] = useState("");
+  const [working, setWorking] = useState(true); //#3.2 
+  const [text, setText] = useState(""); //기본값""주고 유저가 입력하면 payload 저장
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
   const onChangeText = (payload) => setText(payload)
@@ -20,17 +21,22 @@ export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
+
       <View style={styles.header}>
+
         <TouchableOpacity onPress={work}>
-          <Text style={{...styles.btnText, color: working ? "white" : theme.grey }}>Work</Text>
+          <Text style={{...styles.btnText, color: working ? "red" : theme.grey }}>Work</Text>
         </TouchableOpacity>
+
         <TouchableOpacity onPress={travel}>
           <Text style={{...styles.btnText, color: !working ? "white" : theme.grey }}>travel</Text>
         </TouchableOpacity>
+
       </View>
+      
         <View>
-          <TextInput onChangeText={onChangeText} value={text} placeholder={working ? "Add a to Do" : "where do you want to go"} style={styles.input}>
-            
+          <TextInput keyboardType="number-pad " onChangeText={onChangeText} value={text} placeholder={working ? "Add a to Do" : "where do you wanna go"} style={styles.input}>
+            {/* onChangeText : 바뀌면 알려줌 */}
           </TextInput>
         </View>
     </View>
