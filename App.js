584891930c26b66222791e8f0e6dabddc39d2c2b
BlackMarine <wkdeo0000@gmail.com>
2022-01-17 16:22:53 +0900

    #3.3 ToTos 객체 병합 방법 Object.assign({}, [Date.now()] : { text, work: working }  )

--- App.js
+++ App.js
@@ -1,44 +1,81 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, 
-  Text, 
-  View, 
+import {
+  StyleSheet,
+  Text,
+  View,
   TouchableOpacity, //가장 쉽게 work travel 나눠가고 text input까지만 ㅂ
   TouchableHighlight,
-  TouchableWithoutFeedback, //그래픽 UI 변화없이 
-  TextInput //화면에 가장 위에서 일어나는 탭 이벤트를 listen (UI변화x) 애니메이션만없음
+  TouchableWithoutFeedback, //그래픽 UI 변화없이
+  TextInput, //화면에 가장 위에서 일어나는 탭 이벤트를 listen (UI변화x) 애니메이션만없음
   // 1)텍스트 인풋 import 2)<TextInput> 추가
 } from "react-native";
 import { theme } from "./color";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function App() {
-  const [working, setWorking] = useState(true); //#3.2 
+  const [working, setWorking] = useState(true); //#3.2
   const [text, setText] = useState(""); //기본값""주고 유저가 입력하면 payload 저장
+  const [toDos, setToDos] = useState({}); // 배열[] 객체{} 넣기 배열대신 {} 넣음
+  // 설명: Object.assign({}, toDos, {[Date.now()]:{work:true}}); 이런식으로 객체에 추가로 넣을 수 있어서 객체를 사용함
+  // toDos를 변경시키는 것이 아니라 새로운 toDos를 이용해서 변경함
+
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
-  const onChangeText = (payload) => setText(payload)
+  const onChangeText = (payload) => setText(payload);
+
+  const addToDo = () => {
+    alert(text);
+    if (text === "") {
+      return;
+    }
+
+    const newToDos = Object.assign({}, toDos, {
+      [Date.now()]: { text, work: working },
+    });
+    setToDos(newToDos);
+    setText("");
+  };
+
+  
+  console.log(toDos);
+
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
 
       <View style={styles.header}>
-
         <TouchableOpacity onPress={work}>
-          <Text style={{...styles.btnText, color: working ? "red" : theme.grey }}>Work</Text>
+          <Text
+            style={{ ...styles.btnText, color: working ? "red" : theme.grey }}
+          >
+            Work
+          </Text>
         </TouchableOpacity>
 
         <TouchableOpacity onPress={travel}>
-          <Text style={{...styles.btnText, color: !working ? "white" : theme.grey }}>travel</Text>
+          <Text
+            style={{
+              ...styles.btnText,
+              color: !working ? "white" : theme.grey,
+            }}
+          >
+            travel
+          </Text>
         </TouchableOpacity>
-
       </View>
-      
-        <View>
-          <TextInput keyboardType="number-pad " onChangeText={onChangeText} value={text} placeholder={working ? "Add a to Do" : "where do you wanna go"} style={styles.input}>
-            {/* onChangeText : 바뀌면 알려줌 */}
-          </TextInput>
-        </View>
+
+      <View>
+        <TextInput
+          onChangeText={onChangeText}
+          value={text}
+          placeholder={working ? "Add a to Do" : "where do you wanna go"}
+          onSubmitEditing={addToDo}
+          style={styles.input}
+        >
+          {/* onChangeText : 바뀌면 알려줌 */}
+        </TextInput>
+      </View>
     </View>
   );
 }
@@ -68,5 +105,5 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     marginTop: 20,
     fontSize: 18,
-  }
+  },
 });
