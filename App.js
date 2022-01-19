0cf19e21b85854697b67d2f69b5c576b83f0602a
BlackMarine <wkdeo0000@gmail.com>
2022-01-19 15:07:57 +0900

    recap

--- App.js
+++ App.js
@@ -8,12 +8,13 @@ import {
   TouchableWithoutFeedback, //그래픽 UI 변화없이
   TextInput, //화면에 가장 위에서 일어나는 탭 이벤트를 listen (UI변화x) 애니메이션만없음
   // 1)텍스트 인풋 import 2)<TextInput> 추가
-  ScrollView, //#3.4 텍스트입력시 보이도록하기
+  ScrollView,
+  Alert, //#3.4 텍스트입력시 보이도록하기
 } from "react-native";
 import { theme } from "./color";
 import React, { useEffect, useState } from "react";
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import { AntDesign } from '@expo/vector-icons';  //휴지통아이콘
 const STORAGE_KEY = "@toDos";
 
 export default function App() {
@@ -65,6 +66,18 @@ export default function App() {
 
   console.log(toDos);
 
+  const deleteToDo = (key) => {
+    Alert.alert("삭제할거야?", "진짜로?",[
+      {text:"취소"},
+      {text:"삭제", onPress: async() => {
+        const newToDos = {...toDos} //mutating state는 mutate하면안됨
+        delete newToDos[key] //새롭게 만들어줌
+        setToDos(newToDos); //update the state
+        saveToDos(newToDos); //save that action 
+      }}
+    ]);
+  }
+
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -109,6 +122,9 @@ export default function App() {
         {Object.keys(toDos).map((key) => (
           toDos[key].working === working ? <View style={styles.toDo} key={key}>
             <Text style={styles.toDoText}>{toDos[key].text}</Text>
+            <TouchableOpacity onPress={() => deleteToDo(key)}>
+              <AntDesign name="delete" size={18} color={theme.grey} />
+            </TouchableOpacity>
           </View> : null
         ))}
       </ScrollView>
@@ -150,6 +166,9 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     paddingHorizontal: 20,
     borderRadius: 15,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
   toDoText: {
     color: "white",
@@ -157,3 +176,22 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
   },
 });
+
+
+/*
+
+toDO App 만들면서 학습한 개념 
+onSubmitEditing = 키보드done버튼누르면 실행되는 함수
+... es6 문법 다른 아이템을 합치는 기능. 비슷한거 = Object.assign
+keytype
+
+onPress 눌렀다 때는것
+persistence
+touchable
+alert
+react.js 스킬 
+style
+input
+
+
+*/
